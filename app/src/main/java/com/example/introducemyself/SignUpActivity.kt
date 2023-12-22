2b69683a172c1ea32a0331c120bb4ef4e9b08be4
CodeNewbieee <choi2232@hanmail.net>
2023-12-22 16:57:57 +0900

    수정3: 회원가입 작성칸 아래 빨간경고표시, 실시간 입력 확인후 표시 숨김

--- app/src/main/java/com/example/introducemyself/SignUpActivity.kt
+++ app/src/main/java/com/example/introducemyself/SignUpActivity.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 
 class SignUpActivity : AppCompatActivity() {
+    // 초기화 지연 ,
+    val tv_blank1 : TextView by lazy { findViewById(R.id.tv_blank1) }
+    val tv_blank2 : TextView by lazy { findViewById(R.id.tv_blank2) }
+    val tv_blank3 : TextView by lazy { findViewById(R.id.tv_blank3) }
+    val tv_blank4 : TextView by lazy { findViewById(R.id.tv_blank4) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -44,6 +54,25 @@ class SignUpActivity : AppCompatActivity() {
             // 회원가입 버튼 클릭시 메인인 signinActivity로 이동
             // signupActivity는 onPause, Stop, Destroy 순으로 진행되며 사라짐
 
+        }
+
+        // 빨간 경고 글씨 실시간 빈칸여부 체크 후 보여지기
+        nametext.doAfterTextChanged {
+            tv_blank1.isVisible = nametext.text.isEmpty()  // isvisible을 통해서 true, false 확인 true라면 tv_black1의 텍스트가 보이고, false면 안보임
+        }
+        // 아래 if문을 통해서도 가능하지만 위 구문 처럼 한줄로도 가능
+        agetext.doAfterTextChanged {
+            if(agetext.text.isNotEmpty()) {
+                tv_blank2.visibility = View.INVISIBLE
+            } else if(agetext.text.isEmpty()) {
+                tv_blank2.visibility = View.VISIBLE
+            }
+        }
+        idtext.doAfterTextChanged {
+            tv_blank3.isVisible = idtext.text.isEmpty()
+        }
+        passtext.doAfterTextChanged {
+            tv_blank4.isVisible = passtext.text.isEmpty()
         }
 
     }
